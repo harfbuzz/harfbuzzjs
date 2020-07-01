35a9292e27b902948b286940f6bbd34c4b3b322c
Ebrahim Byagowi <ebrahim@gnu.org>
2020-07-01 15:57:17 +0430

    draw text

--- hbjs.js
+++ hbjs.js
@@ -5,19 +5,17 @@ function hbjs(instance) {
   var heapu8 = new Uint8Array(exports.memory.buffer);
   var heapu32 = new Uint32Array(exports.memory.buffer);
   var heapi32 = new Int32Array(exports.memory.buffer);
+  var utf8Decoder = new TextDecoder("utf8");
 
   var HB_MEMORY_MODE_WRITABLE = 2;
 
   function createBlob(blob) {
     var blobPtr = exports.malloc(blob.byteLength);
-    heapu8.set(blob, blobPtr);
-    var ptr = exports.hb_blob_create(blobPtr, blob.byteLength, HB_MEMORY_MODE_WRITABLE, 0, 0);
+    heapu8.set(new Uint8Array(blob), blobPtr);
+    var ptr = exports.hb_blob_create(blobPtr, blob.byteLength, HB_MEMORY_MODE_WRITABLE, blobPtr, exports.free_ptr());
     return {
       ptr: ptr,
-      free: function () {
-        exports.hb_blob_destroy(ptr);
-        exports.free(blobPtr);
-      }
+      destroy: function () { exports.hb_blob_destroy(ptr); }
     };
   }
   
@@ -25,25 +23,43 @@ function hbjs(instance) {
     var ptr = exports.hb_face_create(blob.ptr, index);
     return {
       ptr: ptr,
-      free: function () { exports.hb_face_destroy(ptr); }
+      destroy: function () { exports.hb_face_destroy(ptr); }
     };
   }
 
+  var pathBufferSize = 65536; // should be enough for most glyphs
+  var pathBuffer = exports.malloc(pathBufferSize); // permanently allocated
+
   function createFont(face) {
     var ptr = exports.hb_font_create(face.ptr);
+
+    function glyphToPath(glyphId) {
+      var svgLength = exports.hbjs_glyph_svg(ptr, glyphId, pathBuffer, pathBufferSize);
+      return svgLength > 0 ? utf8Decoder.decode(heapu8.subarray(pathBuffer, pathBuffer + svgLength)) : "";
+    }
+
     return {
       ptr: ptr,
+      glyphToPath: glyphToPath,
+      glyphToJson: function (glyphId) {
+        var path = glyphToPath(glyphId);
+        return path.replace(/([MLQCZ])/g, '|$1 ').split('|').filter(function (x) { return x.length; }).map(function (x) {
+          var row = x.split(/[ ,]/g);
+          // based on format described on https://svgwg.org/specs/paths/#InterfaceSVGPathSegment
+          return { type: row[0], values: row.slice(1).filter(function (x) { return x.length; }).map(function (x) { return +x; }) };
+        });
+      },
       setScale: function (xScale, yScale) {
         exports.hb_font_set_scale(ptr, xScale, yScale);
       },
-      free: function () { exports.hb_font_destroy(ptr); }
+      destroy: function () { exports.hb_font_destroy(ptr); }
     };
   }
 
   var utf8Encoder = new TextEncoder("utf8");
   function createCString(text) {
     var bytes = utf8Encoder.encode(text);
-    var ptr = exports.malloc(text.byteLength);
+    var ptr = exports.malloc(bytes.byteLength);
     heapu8.set(bytes, ptr);
     return {
       ptr: ptr,
@@ -72,17 +88,16 @@ function hbjs(instance) {
           btt: 7
         }[dir] || 0);
       },
-      shape: function (font, features) {
-        // features are not used yet
-        exports.hb_shape(font.ptr, ptr, 0, 0);
+      setClusterLevel: function (level) {
+        exports.hb_buffer_set_cluster_level(ptr, level)
       },
       json: function (font) {
         var length = exports.hb_buffer_get_length(ptr);
         var result = [];
         var infosPtr32 = exports.hb_buffer_get_glyph_infos(ptr, 0) / 4;
         var positionsPtr32 = exports.hb_buffer_get_glyph_positions(ptr, 0) / 4;
-        var infos = heapu32.slice(infosPtr32, infosPtr32 + 5 * length);
-        var positions = heapi32.slice(positionsPtr32, positionsPtr32 + 5 * length);
+        var infos = heapu32.subarray(infosPtr32, infosPtr32 + 5 * length);
+        var positions = heapi32.subarray(positionsPtr32, positionsPtr32 + 5 * length);
         for (var i = 0; i < length; ++i) {
           result.push({
             g: infos[i * 5 + 0],
@@ -95,15 +110,33 @@ function hbjs(instance) {
         }
         return result;
       },
-      free: function () { exports.hb_buffer_destroy(ptr); }
+      destroy: function () { exports.hb_buffer_destroy(ptr); }
     };
+  }
+
+  function shape(font, buffer, features) {
+    // features are not used yet
+    exports.hb_shape(font.ptr, buffer.ptr, 0, 0);
+  }
+
+  function shapeWithTrace(font, buffer, features, stop_at, stop_phase) {
+    var bufLen = 1024 * 1024;
+    var traceBuffer = exports.malloc(bufLen);
+    var featurestr = createCString(features);
+    var traceLen = exports.hbjs_shape_with_trace(font.ptr, buffer.ptr, featurestr.ptr, stop_at, stop_phase, traceBuffer, bufLen);
+    featurestr.free();
+    var trace = utf8Decoder.decode(heapu8.subarray(traceBuffer, traceBuffer + traceLen - 1));
+    exports.free(traceBuffer);
+    return JSON.parse(trace);
   }
 
   return {
     createBlob: createBlob,
     createFace: createFace,
     createFont: createFont,
-    createBuffer: createBuffer
+    createBuffer: createBuffer,
+    shape: shape,
+    shapeWithTrace: shapeWithTrace
   };
 };
 
