bd17dc3156ec78049336d28f9a2eb176d85c9ffe
Simon Cozens <simon@simon-cozens.org>
2020-07-03 14:34:58 +0100

    Document hbjs.js
    Unfortunately jsdoc can't automatically format this into something useful, because jsdoc expects methods to be attached to classes rather than bare objects. But it's still better than nothing.

--- hbjs.js
+++ hbjs.js
@@ -9,20 +9,36 @@ function hbjs(instance) {
 
   var HB_MEMORY_MODE_WRITABLE = 2;
 
+  /**
+  * Create an object representing a Harfbuzz blob.
+  * @param {string} blob A blob of binary data (usually the contents of a font file).
+  **/
   function createBlob(blob) {
     var blobPtr = exports.malloc(blob.byteLength);
     heapu8.set(new Uint8Array(blob), blobPtr);
     var ptr = exports.hb_blob_create(blobPtr, blob.byteLength, HB_MEMORY_MODE_WRITABLE, blobPtr, exports.free_ptr());
     return {
       ptr: ptr,
+      /**
+      * Free the object.
+      */
       destroy: function () { exports.hb_blob_destroy(ptr); }
     };
   }
-  
+
+  /**
+  * Create an object representing a Harfbuzz face.
+  * @param {object} blob An object returned from `createBlob`.
+  * @param {number} index The index of the font in the blob. (0 for most files,
+  *  or a 0-indexed font number if the `blob` came form a TTC/OTC file.)
+  **/
   function createFace(blob, index) {
     var ptr = exports.hb_face_create(blob.ptr, index);
     return {
       ptr: ptr,
+      /**
+      * Free the object.
+      */
       destroy: function () { exports.hb_face_destroy(ptr); }
     };
   }
@@ -30,9 +46,17 @@ function hbjs(instance) {
   var pathBufferSize = 65536; // should be enough for most glyphs
   var pathBuffer = exports.malloc(pathBufferSize); // permanently allocated
 
+  /**
+  * Create an object representing a Harfbuzz font.
+  * @param {object} blob An object returned from `createFace`.
+  **/
   function createFont(face) {
     var ptr = exports.hb_font_create(face.ptr);
 
+    /**
+    * Return a glyph as an SVG path string.
+    * @param {number} glyphId ID of the requested glyph in the font.
+    **/
     function glyphToPath(glyphId) {
       var svgLength = exports.hbjs_glyph_svg(ptr, glyphId, pathBuffer, pathBufferSize);
       return svgLength > 0 ? utf8Decoder.decode(heapu8.subarray(pathBuffer, pathBuffer + svgLength)) : "";
@@ -41,17 +65,30 @@ function hbjs(instance) {
     return {
       ptr: ptr,
       glyphToPath: glyphToPath,
+      /**
+      * Return a glyph as a JSON path string
+      * based on format described on https://svgwg.org/specs/paths/#InterfaceSVGPathSegment
+      * @param {number} glyphId ID of the requested glyph in the font.
+      **/
       glyphToJson: function (glyphId) {
         var path = glyphToPath(glyphId);
         return path.replace(/([MLQCZ])/g, '|$1 ').split('|').filter(function (x) { return x.length; }).map(function (x) {
           var row = x.split(/[ ,]/g);
-          // based on format described on https://svgwg.org/specs/paths/#InterfaceSVGPathSegment
           return { type: row[0], values: row.slice(1).filter(function (x) { return x.length; }).map(function (x) { return +x; }) };
         });
       },
+      /**
+      * Set the font's scale factor, affecting the position values returned from
+      * shaping.
+      * @param {number} xScale Units to scale in the X dimension.
+      * @param {number} yScale Units to scale in the Y dimension.
+      **/
       setScale: function (xScale, yScale) {
         exports.hb_font_set_scale(ptr, xScale, yScale);
       },
+      /**
+      * Free the object.
+      */
       destroy: function () { exports.hb_font_destroy(ptr); }
     };
   }
@@ -68,18 +105,34 @@ function hbjs(instance) {
     };
   }
 
+  /**
+  * Create an object representing a Harfbuzz buffer.
+  **/
   function createBuffer() {
     var ptr = exports.hb_buffer_create();
     return {
       ptr: ptr,
+      /**
+      * Add text to the buffer.
+      * @param {string} text Text to be added to the buffer.
+      **/
       addText: function (text) {
         var str = createCString(text);
         exports.hb_buffer_add_utf8(ptr, str.ptr, str.length, 0, str.length);
         str.free();
       },
+      /**
+      * Set buffer script, language and direction.
+      *
+      * This needs to be done before shaping.
+      **/
       guessSegmentProperties: function () {
         return exports.hb_buffer_guess_segment_properties(ptr);
       },
+      /**
+      * Set buffer direction explicitly.
+      * @param {string} direction: One of "ltr", "rtl", "ttb" or "btt"
+      */
       setDirection: function (dir) {
         exports.hb_buffer_set_direction(ptr, {
           ltr: 4,
@@ -88,9 +141,29 @@ function hbjs(instance) {
           btt: 7
         }[dir] || 0);
       },
+      /**
+      * Set the Harfbuzz clustering level.
+      *
+      * Affects the cluster values returned from shaping.
+      * @param {number} level: Clustering level. See the Harfbuzz manual chapter
+      * on Clusters.
+      **/
       setClusterLevel: function (level) {
         exports.hb_buffer_set_cluster_level(ptr, level)
       },
+      /**
+      * Return the buffer contents as a JSON object.
+      *
+      * After shaping, this function will return an array of glyph information
+      * objects. Each object will have the following attributes:
+      *
+      *   - g: The glyph ID
+      *   - cl: The cluster ID
+      *   - ax: Advance width (width to advance after this glyph is painted)
+      *   - ay: Advance height (height to advance after this glyph is painted)
+      *   - dx: X displacement (adjustment in X dimension when painting this glyph)
+      *   - d5: Y displacement (adjustment in Y dimension when painting this glyph)
+      **/
       json: function (font) {
         var length = exports.hb_buffer_get_length(ptr);
         var result = [];
@@ -110,14 +183,44 @@ function hbjs(instance) {
         }
         return result;
       },
+      /**
+      * Free the object.
+      */
       destroy: function () { exports.hb_buffer_destroy(ptr); }
     };
   }
 
+  /**
+  * Shape a buffer with a given font.
+  *
+  * This returns nothing, but modifies the buffer.
+  *
+  * @param {object} font: A font returned from `createFont`
+  * @param {object} buffer: A buffer returned from `createBuffer` and suitably
+  *   prepared.
+  * @param {object} features: (Currently unused).
+  */
   function shape(font, buffer, features) {
-    // features are not used yet
     exports.hb_shape(font.ptr, buffer.ptr, 0, 0);
   }
+
+  /**
+  * Shape a buffer with a given font, returning a JSON trace of the shaping process.
+  *
+  * This function supports "partial shaping", where the shaping process is
+  * terminated after a given lookup ID is reached. If the user requests the function
+  * to terminate shaping after an ID in the GSUB phase, GPOS table lookups will be
+  * processed as normal.
+  *
+  * @param {object} font: A font returned from `createFont`
+  * @param {object} buffer: A buffer returned from `createBuffer` and suitably
+  *   prepared.
+  * @param {object} features: A dictionary of OpenType features to apply.
+  * @param {number} stop_at: A lookup ID at which to terminate shaping.
+  * @param {number} stop_phase: Either 0 (don't terminate shaping), 1 (`stop_at`
+      refers to a lookup ID in the GSUB table), 2 (`stop_at` refers to a lookup
+      ID in the GPOS table).
+  */
 
   function shapeWithTrace(font, buffer, features, stop_at, stop_phase) {
     var bufLen = 1024 * 1024;
