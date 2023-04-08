d74d8d6ad75ac549c4e8288c2a2144fee232f822
Andreas Lind <andreaslindpetersen@gmail.com>
2023-04-08 23:00:12 +0200

    Fix: Destroy the right pointer in collectUnicodes

--- hbjs.js
+++ hbjs.js
@@ -142,7 +142,7 @@ function hbjs(instance) {
         var unicodeSetPtr = exports.hb_set_create();
         exports.hb_face_collect_unicodes(ptr, unicodeSetPtr);
         var result = typedArrayFromSet(unicodeSetPtr, Uint32Array);
-        exports.hb_set_destroy(ptr);
+        exports.hb_set_destroy(unicodeSetPtr);
         return result;
       },
       /**
