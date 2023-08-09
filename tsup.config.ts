6be934af9e7bb2a9c99445503276c7ad18214e74
lynn <jcignorance@outlook.com>
2023-08-09 18:46:28 +0800

    refine

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-  entry: ['src/index.ts'],
+export default defineConfig([{
+  entry: ['src/client.ts'],
+  outDir: 'dist/client',
+  minify: true,
+  splitting: false,
+  sourcemap: true,
+  target: 'es2015',
+  dts: true,
+  format: ['esm', 'iife']
+}, {
+  entry: ['src/node.ts'],
+  outDir: 'dist/node',
   minify: true,
   splitting: false,
   sourcemap: true,
   target: 'node16',
   dts: true,
   format: ['esm', 'cjs']
-})
+}])
