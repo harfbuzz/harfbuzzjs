97601c4e53c7e38c7b2b12487991850ff227ab5b
lynn <jcignorance@outlook.com>
2023-08-09 18:46:14 +0800

    refine

---
+++ playground/src/main.ts
@@ -0,0 +1,59 @@
+import { hb as hbjs } from 'harfbuzzjs/client'
+import init from './assets/hb.wasm?init'
+import fontUrl from './assets/NotoSans-Regular.ttf?url'
+
+main().catch(e => {
+  console.error(e)
+})
+
+async function main () {
+  const wasm = await init({})
+  const res = await fetch(fontUrl)
+  const data = await res.arrayBuffer()
+  document.body.innerText = JSON.stringify(
+    preview(hbjs(wasm), new Uint8Array(data), ''),
+    undefined,
+    2
+  )
+  document.body.style.whiteSpace = 'pre'
+}
+
+function preview (hb: Awaited<ReturnType<typeof hbjs>>, data: ArrayBuffer, text: string) {
+  const blob = hb.createBlob(data)
+  const face = hb.createFace(blob, 0)
+  const font = hb.createFont(face)
+  font.setScale(1000, 1000)
+  const buffer = hb.createBuffer()
+  buffer.addText(text || 'abc')
+  buffer.guessSegmentProperties()
+  hb.shape(font, buffer)
+  const shape = buffer.json()
+  const glyphs = shape
+    .reduce<{
+      [p: number]: {
+        name: string
+        path: string
+        json: {
+          type: string
+          values: number[]
+        }[]
+      }
+    }>((res, item) => {
+      if (item.g) return res
+
+      res[item.g] = {
+        name: font.glyphName(item.g),
+        path: font.glyphToPath(item.g),
+        json: font.glyphToJson(item.g)
+      }
+      return res
+    }, {})
+
+  const unicodes = face.collectUnicodes()
+
+  return {
+    shape,
+    glyphs,
+    unicodes
+  }
+}
