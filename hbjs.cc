32a8babe0c986041190f60f0c7b968ddca6a84e7
Khaled Hosny <khaled@aliftype.com>
2024-03-24 03:24:50 +0200

    Re-implement hbjs_glyph_svg in JS

--- hbjs.cc
+++ hbjs.cc
@@ -2,9 +2,6 @@
 
 HB_BEGIN_DECLS
 
-int
-hbjs_glyph_svg (hb_font_t *font, hb_codepoint_t glyph, char *buf, unsigned buf_size);
-
 unsigned
 hbjs_shape_with_trace (hb_font_t *font, hb_buffer_t* buf,
                        char* featurestring,
@@ -69,81 +66,6 @@ _user_data_printf (user_data_t *data, const char *format, ...)
   memcpy (data->str + data->consumed, buf, len);
   data->consumed += len;
 #undef BUFSIZE
-}
-
-static void
-move_to (hb_draw_funcs_t *dfuncs, user_data_t *draw_data, hb_draw_state_t *,
-	 float to_x, float to_y,
-	 void *)
-{
-  _user_data_printf (draw_data, "M%g,%g", (double)to_x, (double)to_y);
-}
-
-static void
-line_to (hb_draw_funcs_t *dfuncs, user_data_t *draw_data, hb_draw_state_t *,
-	 float to_x, float to_y,
-	 void *)
-{
-  _user_data_printf (draw_data, "L%g,%g", (double)to_x, (double)to_y);
-}
-
-static void
-quadratic_to (hb_draw_funcs_t *dfuncs, user_data_t *draw_data, hb_draw_state_t *,
-	      float control_x, float control_y,
-	      float to_x, float to_y,
-	      void *)
-{
-  _user_data_printf (draw_data, "Q%g,%g %g,%g",
-                     (double)control_x,
-                     (double)control_y,
-                     (double)to_x,
-                     (double)to_y);
-}
-
-static void
-cubic_to (hb_draw_funcs_t *dfuncs, user_data_t *draw_data, hb_draw_state_t *,
-	  float control1_x, float control1_y,
-	  float control2_x, float control2_y,
-	  float to_x, float to_y,
-	  void *)
-{
-  _user_data_printf (draw_data, "C%g,%g %g,%g %g,%g",
-                     (double)control1_x,
-                     (double)control1_y,
-                     (double)control2_x,
-                     (double)control2_y,
-                     (double)to_x,
-                     (double)to_y);
-}
-
-static void
-close_path (hb_draw_funcs_t *dfuncs, user_data_t *draw_data, hb_draw_state_t *, void *)
-{
-  _user_data_printf (draw_data, "Z");
-}
-
-static hb_draw_funcs_t *funcs = 0;
-
-int
-hbjs_glyph_svg (hb_font_t *font, hb_codepoint_t glyph, char *buf, unsigned buf_size)
-{
-  if (funcs == 0) /* not the best pattern for multi-threaded apps which is not a concern here */
-  {
-    funcs = hb_draw_funcs_create (); /* will be leaked */
-    hb_draw_funcs_set_move_to_func (funcs, (hb_draw_move_to_func_t) move_to, nullptr, nullptr);
-    hb_draw_funcs_set_line_to_func (funcs, (hb_draw_line_to_func_t) line_to, nullptr, nullptr);
-    hb_draw_funcs_set_quadratic_to_func (funcs, (hb_draw_quadratic_to_func_t) quadratic_to, nullptr, nullptr);
-    hb_draw_funcs_set_cubic_to_func (funcs, (hb_draw_cubic_to_func_t) cubic_to, nullptr, nullptr);
-    hb_draw_funcs_set_close_path_func (funcs, (hb_draw_close_path_func_t) close_path, nullptr, nullptr);
-  }
-
-  user_data_t draw_data(buf, buf_size);
-  hb_font_draw_glyph (font, glyph, funcs, &draw_data);
-  if (draw_data.failure)
-    return -1;
-
-  buf[draw_data.consumed] = '\0';
-  return draw_data.consumed;
 }
 
 static hb_bool_t do_trace (hb_buffer_t *buffer,
@@ -243,19 +165,3 @@ hbjs_shape_with_trace (hb_font_t *font, hb_buffer_t* buf,
   user_data.str[user_data.consumed-1] = '\0';
   return user_data.consumed;
 }
-
-#ifdef MAIN
-#include <stdio.h>
-int main() {
-    hb_blob_t *blob = hb_blob_create_from_file ("/home/ebrahim/Desktop/harfbuzzjs/harfbuzz/test/subset/data/fonts/Roboto-Regular.ttf");
-    hb_face_t *face = hb_face_create (blob, 0);
-    hb_blob_destroy (blob);
-    hb_font_t *font = hb_font_create (face);
-    hb_face_destroy (face);
-    char buf[1024];
-    buf[0] = '\0';
-    printf ("%d %d\n", hb_blob_get_length (blob), hbjs_ot_glyph_svg (font, 0, buf, sizeof (buf)));
-    puts (buf);
-    hb_font_destroy (font);
-}
-#endif
