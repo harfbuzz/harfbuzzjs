4679cb31b9e450375db5d0d2564ec724ccc1eab0
梁海 Liang Hai <lianghai@gmail.com>
2026-07-01 01:45:08 +0200

    Use .getGlyphPositions() in examples
    .getGlyphInfosAndPositions() is quite cumbersome in practice because of the Partial<GlyphPosition>.

--- examples/harfbuzz.example.node.js
+++ examples/harfbuzz.example.node.js
@@ -2,17 +2,29 @@ import fs from "node:fs";
 import path from "node:path";
 import * as hb from "harfbuzzjs";
 
+// Load data from a font file:
 const nodeBuffer = fs.readFileSync(
   path.join(import.meta.dirname, "../test/fonts/noto/NotoSans-Regular.ttf"),
 );
 
+// Create a HarfBuzz font object from the data:
 const blob = new hb.Blob(nodeBuffer);
 const face = new hb.Face(blob);
 const font = new hb.Font(face);
 
+// Shape text in a HarfBuzz buffer with the font:
 const buffer = new hb.Buffer();
 buffer.addText("abc");
 buffer.guessSegmentProperties();
 hb.shape(font, buffer);
 
-console.log(buffer.getGlyphInfosAndPositions());
+// Enumerate the resulted glyphs in the buffer:
+const infos = buffer.getGlyphInfos();
+const positions = buffer.getGlyphPositions();
+for (const [index, glyph] of infos.entries()) {
+  const gid = glyph.codepoint; // Glyph ID despite the property name
+  console.log(
+    font.glyphToPath(gid), // SVG path
+    positions[index], // xAdvance, yAdvance, xOffset, yOffset
+  );
+}
